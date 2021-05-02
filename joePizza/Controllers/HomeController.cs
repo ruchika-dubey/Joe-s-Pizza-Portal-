63035b45b01a3fff69a18e37ab8d89be7aaa5acc
Ruchika Dubey <79623739+ruchika-dubey@users.noreply.github.com>
2021-05-03 02:51:17 +0530

    code

---
+++ joePizza/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using JoePizza.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JoePizza.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+       
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            
+            
+            return View();
+
+        }
+        public IActionResult Checkout()
+        {
+
+
+            return View();
+
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
